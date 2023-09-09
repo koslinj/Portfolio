a1296f4004d2aad7a213a1925240083c7fe45456
koslinj <koslinj@gmail.com>
2023-09-09 14:05:05 +0200

    footer margin change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='mt-40 mb-2 text-center font-title tracking-[0.2rem]'>Created by Jan Koślin ©2023</div>
+    <div className='mt-32 mb-12 text-center font-title tracking-[0.2rem]'>Created by Jan Koślin ©2023</div>
   )
 }
 
